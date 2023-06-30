d38fecd00c53b027f8a6cd398195ac6f9419be3f
anant134 <46345092+anant134@users.noreply.github.com>
2023-06-30 15:28:11 +0530

    Delete Areas directory

--- Areas/Admin/Client/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api_root: "https://www.apsmumbai.com/api",
-};
--- Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace aspm.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
